89174faced4b1b3652873db44128d8f81d5e0a92
HanChun <1136741748@qq.com>
2016-03-11 20:24:20 +0800

    test

---
+++ LessionOnline/demo02.java
@@ -0,0 +1,15 @@
+package StringAndValue;
+/**
+ * @author HANXT
+ *	简单的加密
+ *	用toCharArray()
+ */
+
+public class demo02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
