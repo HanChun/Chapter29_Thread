c6f06996abdcf510a5ea30b35df39ea69f8637cb
HanChun <1136741748@qq.com>
2016-04-09 16:14:15 +0800

    0409

--- Java/src/Exception/ExceptionDemo02.java
+++ Java/src/Exception/ExceptionDemo02.java
@@ -9,8 +9,12 @@ package Exception;
  * 
  * 发现打印的结果中只有异常的名称，却没有异常的信息（toString是打印异常的信息和名称）
  * 如何定义异常信息呢？
+ * 
+ * 自定义类必须是自定义类继承Exception；
  */
-class FuShuException extends Exception{
+
+
+/*class FuShuException extends Exception{
 	private String msg ;
 	FuShuException(String msg){
 		this.msg = msg;
@@ -18,16 +22,35 @@ class FuShuException extends Exception{
 	public String getMsg() {
 		return msg;
 	}
-	public void setMsg(String msg) {
-		this.msg = msg;
+}*/
+
+class FuShuException extends Exception{
+	//在出现异常的时候怎么没有用Throwable呢，其实也是可以的
+	//继承Exception原因：异常体系一个特点，因为异常类和异常对象都需要被抛出，
+	//这个可抛型是Throwable这个体系中的独有特点。只有这个体系中的类和对象才可以被throws和throw操作；
+	private int value;
+	FuShuException(){
+		super();
 	}
-	
+	FuShuException(String msg,int value){
+		super(msg);
+		this.value = value;
+	}
+	public int getValue(){
+		return value;
+	}
 }
 
 class Demo02{
+	/*
+	 *  int div(int a,int b){
+	 * 		return a/b;
+	 * }
+	 * 下面是修正样子
+	 */
 	int div(int a ,int b) throws FuShuException{
 		if(b<0)
-			throw new FuShuException("出现了除数是负数的情况");//手动通过throw关键字抛出自定义异常对象
+			throw new FuShuException("出现了除数是负数的情况",b);//手动通过throw关键字抛出自定义异常对象
 		
 		return a/b;
 	}
@@ -44,7 +67,9 @@ public class ExceptionDemo02 {
 		} catch (FuShuException e) {
 			// TODO Auto-generated catch block
 			System.out.println(e.toString());
-			System.out.println("除数出现负数了");
+			System.out.println(e.getMessage());
+			System.out.println("除数出现负数了"+e.getValue());
+			
 		}
 			System.out.println("over");
 	}
@@ -71,10 +96,15 @@ class Exception extends Throwable{
 		super(message);
 	}
 }
-//new Exception("haha").getMessage();
+
+	//new Exception("haha").getMessage();
 */
 
-
+/*throws和throw的区别：
+ * throws使用在函数上，throw使用在函数内
+ * throws后面跟的是异常类，可以跟多个，用逗号隔开。
+ * throw后面跟的是异常对象
+ */
 
 
 
---
+++ Java/src/Exception/ExceptionDemo03.java
@@ -0,0 +1,30 @@
+package Exception;
+/*Exception中有一个特殊的子类异常RuntimeException运行时异常
+ * 如果在函数内抛出该异常，函数上可以不用声明，编译一样通过。
+ * 如果在函数上声明了该异常，调用者可以不用处理，编译一样通过
+ * 
+ * 之所以不用在函数上声明，是因为不需要调用者的处理；
+ * 就是希望在此runtimeException发生时，程序停止；希望停止程序后，程序员对代码进行修正；
+ */
+class Demo03{
+	int div(int a, int b) throws ArithmeticException{
+		/*if (b==0)
+			throw new ArithmeticException("被零除了") ;*/
+		// throw new Exception;  如果抛它的话函数上必须要声明（正常情况下函数内抛异常函数上都要声明一下）
+		return a/b;
+	}
+}
+
+public class ExceptionDemo03 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo03 d = new Demo03();
+		
+		int x = d.div(4, 0);
+		System.out.println("x= " + x );
+		
+		System.out.println("over");
+		
+	}
+
+}
