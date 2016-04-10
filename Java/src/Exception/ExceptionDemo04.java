b5a97997050fbbbf3b7b9d85d0c06f3365998008
HanChun <1136741748@qq.com>
2016-04-10 08:56:08 +0800

    0410

--- Java/src/Exception/ExceptionDemo04.java
+++ Java/src/Exception/ExceptionDemo04.java
@@ -1,20 +1,28 @@
 package Exception;
+
 /*
  * 防止NullPointerException
  * 
  * 关于RuntimeException;
  * 自定义异常时，如果该异常发生时，就无法继续进行运算，就让自定义异常继承RuntimeException;
+ * 
+ * 对于异常分为两种：
+ * 1、编译时被检测的异常
+ * 2、编译时不被检测的异常（运行时异常，RuntimeException以及其子类）
+ * 
  */
-class FuShuException extends RuntimeException{
-	FuShuException(String msg){
-		super(msg);
-	}
-}
+
+  class FuShuException1 extends RuntimeException{
+ 	FuShuException1(String msg){
+ 		super(msg);
+ 	}
+ }
+ 
 
 class Demo04{
-	int div(int a, int b){
+	int div(int a, int b){	//throws ArithmeticException
 		if(b<0)
-			throw new FuShuException("出现了除数为负数");
+			throw new FuShuException1("出现了除数为负");
 		if(b==0)
 			throw new ArithmeticException("被零除了");
 		return a/b;
@@ -30,4 +38,13 @@ class Person{
 		else
 			System.out.println("NO");
 	}
+}
+
+public class ExceptionDemo04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
---
+++ Java/src/Exception/ExceptionDemo05.java
@@ -0,0 +1,105 @@
+package Exception;
+/*
+ * 毕老师用电脑上课;
+ */ 
+class ComputerBule extends Exception{
+	ComputerBule(String message){
+		super(message);
+	}
+}
+class ComputerBroke extends Exception{
+	ComputerBroke(String message){
+		super(message);
+	}
+}
+class MaoYanException extends RuntimeException{
+	MaoYanException(String message){
+		super(message);		
+	}
+}
+class NoPlanException extends Exception{
+	NoPlanException(String message){
+		super(message);
+	}
+}
+
+class Computer	{
+	private int state = 1; 
+	
+	public void run() throws ComputerBule, ComputerBroke{
+		if(state == 2)
+				throw new ComputerBule("蓝屏了");
+		if(state == 3)
+				throw new MaoYanException("冒烟了");
+		if(state == 4)
+				throw new ComputerBroke("坏啦");
+		System.out.println("电脑运行");
+	}
+	public void reset(){
+		state =1;
+		System.out.println("电脑重启");
+	}
+}
+
+
+class Teacher{
+	private String name;
+	private Computer cmpt;
+	
+	Teacher(String name){
+		this.name =name;
+		cmpt = new Computer();
+	}
+	
+	public void run() throws NoPlanException{
+		try {
+			cmpt.run();
+			System.out.println("讲课");
+		} catch (ComputerBule e) {
+			// TODO Auto-generated catch block
+			cmpt.reset();
+		} catch (ComputerBroke e) {
+			dotest();
+			throw new NoPlanException("课时无法继续");//如果上个问题真发生了，其就直接抛出去了，不会继续执行了，so dotest()放上面
+			
+		}
+		System.out.println("讲课");
+	}
+	public void dotest(){
+		System.out.println("做练习吧。。。");
+	}
+
+}
+public class ExceptionDemo05 {
+	public static void main(String[] args){
+		Teacher t = new Teacher("毕老师");
+		try {
+			t.run();
+		} catch (NoPlanException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("换电脑或者放假");
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
