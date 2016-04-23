7e3ca73f135c3a74af67bfe510090f8cd85994dd
HanChun <1136741748@qq.com>
2016-04-23 16:39:15 +0800

    0423

--- Java/src/ReflectApp/NoteBookMain.java
+++ Java/src/ReflectApp/NoteBookMain.java
@@ -1,19 +1,55 @@
 package ReflectApp;
-
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class NoteBookMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-	/*	NoteBook book = new NoteBook(); 
+		NoteBook book = new NoteBook(); 
+		/*
 		book.run();
 		book.useUSB(null);
 		book.useUSB(new MouseByUSB());*/
 		
-		//通过反射的方式重新设计应用程序，一提高更好的拓展性
+		//通过反射的方式重新设计应用程序，以提高更好的拓展性
 		File configFile = new File("tempfile\\usb.properties");
+		if(!configFile.exists()){
+			configFile.createNewFile();
+		}
+		//读取配置文件
+		FileReader fr = new FileReader(configFile);
+		//为了获取键值信息方便，建立Properties
+		Properties prop = new Properties();
+		prop.load(fr);
 		
-		
+		for(int x = 1; x<=prop.size(); x++){
+			 String className = prop.getProperty("usb"+x);
+			 Class clazz = Class.forName(className);
+			 USB usb = (USB)clazz.newInstance();
+			 book.useUSB(usb);			 
+		}		
+		fr.close();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Java/src/exe/Main01.java
+++ Java/src/exe/Main01.java
@@ -9,22 +9,22 @@ public class Main01 {
 		int m = sc.nextInt();
 		int n = sc.nextInt();
 		if(m>=100 && m<=n && n<=999){
-		
+			
+			int count = 0;
 			for(int i =m ;i<=n ;i++){
-				int count = 0;
-				
+								
 				int a = i/100;//百位
 				int b = i%10;//个位
 				int c = (i/10)%10;//十位
 				int sum =a*a*a+b*b*b +c*c*c;
 				boolean flag = (sum==i);
 				if(flag){
-					System.out.println(sum+".......");
+					System.out.print(sum+" ");
 					count++;
-				}
-				
+				}				
 			}
-			
+			if(count==0)
+				System.out.println("no");
 			
 		}
 	}
